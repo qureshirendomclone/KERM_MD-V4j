0bdd3ad18bdcd3351fe34396b0359850f6034d5e
qureshirendomclone <mustafaqureshi2206666@gmail.com>
2025-03-11 05:42:55 +0430

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "aBYDFRTY#XsFdDMpHEm2qoetc6IH0HZkW9TaCruabqfTRMOyAJmQ",
+SESSION_ID: process.env.SESSION_ID || "zEoGxThK#BYcVyPP2sgawrxXawtnwPo06gJsc1ToxduB6D7XCjTw",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
